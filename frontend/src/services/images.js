cd5db4ed994085b8f7873f0e29b72c5b4a59a8c1
Juha Myllari <jmyllari@gmail.com>
2019-04-19 20:03:19 +0300

    prepare for Heroku

--- frontend/src/services/images.js
+++ frontend/src/services/images.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const url = 'http://localhost:3000/api'
+// const url = 'http://localhost:3000/api'
+const url = '/api'
 
 const send = (image, width, height) => {
   // console.log(`sending image, image is ${image}`)
